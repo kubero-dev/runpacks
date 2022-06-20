42a1ade3c5354d314a7cf4c89502b516a87a6a5f
Gianni Carafa <gianni.carafa@srf.ch>
2022-06-20 22:31:18 +0200

    add git images

--- node/builder/init.sh
+++ node/builder/init.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-# whipe the app dir
-rm -rf /app/* /app/.*
+#echo "========= load deployment keys"
+#eval `ssh-agent`
+#ssh-add /root/.ssh/*
+#ssh-keyscan github.com >>~/.ssh/known_hosts
+
+echo "========== whipe the app dir"
+rm -rf /app/* /app/.* >> /dev/null 2>&1
 
 # fetch code from git
+echo "========== Clone Repository from $GIT_REPOSITORY"
 cd /app 
 git clone $GIT_REPOSITORY .
 rm -rf .git
 
+echo "========== Run npm install"
 # install node dependencies
-npm install
+
+if [ -f "yarn.lock" ]; then
+    yarn install
+else 
+    npm install
+fi
