8e9fa36d709a9291759f1aa87cc42219921533c8
Gianni Carafa <gianni.carafa@srf.ch>
2022-08-31 22:49:37 +0200

    switch to default images

---
+++ packs/base/build/init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#echo "========= load deployment keys"
+#eval `ssh-agent`
+#ssh-add /root/.ssh/*
+#ssh-keyscan github.com >>~/.ssh/known_hosts
+
+echo "========== whipe the app dir"
+rm -rf /app/* /app/.* >> /dev/null 2>&1
+
+echo "========== Clone Repository from $GIT_REPOSITORY"
+cd /app 
+git clone $GIT_REPOSITORY .
+#git checkout $GIT_BRANCH
+rm -rf .git
+
+echo "========== write startupscripts baed on Procfile"
+BUILD_CMD=$(cat Procfile | grep build | awk -F  ": " '{print $2}')
+echo -n $BUILD_CMD > init-build.sh
+
+WEB_CMD=$(cat Procfile | grep web | awk -F  ": " '{print $2}') 
+echo -n $WEB_CMD > init-web.sh
+
+WORKER_CMD=$(cat Procfile | grep worker | awk -F  ": " '{print $2}')
+echo -n $WORKER_CMD > init-worker.sh
+
+chmod +x init-*.sh
